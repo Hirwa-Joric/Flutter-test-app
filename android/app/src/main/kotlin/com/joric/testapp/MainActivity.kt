367176c0a7fed9fe1ea43ce70ba53f4f7f22640d
Hirwa-Joric <hirwajoric@gmail.com>
2024-02-06 10:48:29 +0200

    second commit

---
+++ android/app/src/main/kotlin/com/joric/testapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.joric.testapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("what's up"),
+    ),
+  ),
+));
